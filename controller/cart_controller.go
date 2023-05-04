ce346d8f467d464717fd82683bab034ddf050012
widhofaisal <widhofaisal26@gmail.com>
2023-05-04 18:05:20 +0700

    auto increment quantity done

--- controller/cart_controller.go
+++ controller/cart_controller.go
@@ -1,9 +1,6 @@
 package controller
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	_ "fmt"
 	"net/http"
 	"strconv"
@@ -38,52 +35,25 @@ func AddCart(c echo.Context) error {
 	isExist_ProductinCart := config.DB.Where("id_product=?", cart.Id_product).First(&cart2)
 	if isExist_ProductinCart.Error == nil {
 		// id_product is exist in table cart
-		//json_map has the JSON Payload decoded into a map
 
-		payload, errA := json.Marshal(cart)
-		if errA != nil {
-			panic(errA)
-		}
-
-		// Membuat request
-		req, errB := http.NewRequest("PUT", "http://127.0.0.1:8000/carts", bytes.NewBuffer(payload))
-		if errB != nil {
-			panic(errB)
-		}
-
-		// Menambahkan header "Authorization"
-		req.Header.Set("Authorization", c.Request().Header.Get("Authorization"))
-
-		// Mengirim request ke server
-		client := &http.Client{}
-		res, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		defer res.Body.Close()
-
-		// Menampilkan response dari server
-		fmt.Println("response Status:", res.Status)
-		fmt.Println("response Headers:", res.Header)
-		buf := new(bytes.Buffer)
-		_, err = buf.ReadFrom(res.Body)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("response Body:", buf.String())
-
+		cart2.Quantity += cart.Quantity
+		config.DB.Save(&cart2)
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success update quantity",
+			"user":    cart2,
+		})
 	}
 
 	// fill price
 	cart.Price = product.Price
 
-	// err := config.DB.Save(&cart).Error
-	// if err != nil {
-	// 	return c.JSON(http.StatusBadRequest, map[string]interface{}{
-	// 		"message": "failed add cart",
-	// 		"error":   err.Error(),
-	// 	})
-	// }
+	err := config.DB.Save(&cart).Error
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "failed add cart",
+			"error":   err.Error(),
+		})
+	}
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"message": "success create cart",
