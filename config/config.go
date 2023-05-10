7d7bbf261e9353ce2e8d9c4ad93b1b0fdc82e3fc
widhofaisal <widhofaisal26@gmail.com>
2023-05-10 11:08:51 +0700

    change config to production

--- config/config.go
+++ config/config.go
@@ -26,31 +26,41 @@ type Config struct {
 }
 
 func InitDB() {
-	config := Config{
-		DB_Username: "root",
-		DB_Password: "",
-		DB_Port:     "3306",
-		DB_Host:     "localhost",
-		DB_Name:     "kasircafe_db",
-	}
-
-	// config for connect rds
-	// use this when deploy using docker
-	// config_rds := Config{
-	// 	DB_Username: "ownerwidho",
-	// 	DB_Password: "kasircafeceria123",
+	// <start> FOR DEVELOPMENT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	// config := Config{
+	// 	DB_Username: "root",
+	// 	DB_Password: "",
 	// 	DB_Port:     "3306",
-	// 	DB_Host:     "db-kasircafe.ctbpmx2n7zth.ap-southeast-2.rds.amazonaws.com",
-	// 	DB_Name:     "db_kasircafe",
+	// 	DB_Host:     "localhost",
+	// 	DB_Name:     "kasircafe_db",
 	// }
 
+	// connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	// 	config.DB_Username,
+	// 	config.DB_Password,
+	// 	config.DB_Host,
+	// 	config.DB_Port,
+	// 	config.DB_Name,
+	// )
+	// <end> FOR DEVELOPMENT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	// <start> FOR PRODUCTION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	config_rds := Config{
+		DB_Username: "ownerwidho",
+		DB_Password: "kasircafeceria123",
+		DB_Port:     "3306",
+		DB_Host:     "db-kasircafe.ctbpmx2n7zth.ap-southeast-2.rds.amazonaws.com",
+		DB_Name:     "db_kasircafe",
+	}
+
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config.DB_Username,
-		config.DB_Password,
-		config.DB_Host,
-		config.DB_Port,
-		config.DB_Name,
+		config_rds.DB_Username,
+		config_rds.DB_Password,
+		config_rds.DB_Host,
+		config_rds.DB_Port,
+		config_rds.DB_Name,
 	)
+	// <end> FOR PRODUCTION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 	var err error
 	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
