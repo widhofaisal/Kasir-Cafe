a1dbd224619e5f96298a6d6677beb98fd765a50b
widhofaisal <widhofaisal26@gmail.com>
2023-05-12 18:17:41 +0700

    modify git ignore

--- config/config.go
+++ config/config.go
@@ -27,39 +27,39 @@ type Config struct {
 
 func InitDB() {
 	// <start> FOR DEVELOPMENT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	// config := Config{
-	// 	DB_Username: "root",
-	// 	DB_Password: "",
-	// 	DB_Port:     "3306",
-	// 	DB_Host:     "localhost",
-	// 	DB_Name:     "kasircafe_db",
-	// }
-
-	// connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-	// 	config.DB_Username,
-	// 	config.DB_Password,
-	// 	config.DB_Host,
-	// 	config.DB_Port,
-	// 	config.DB_Name,
-	// )
-	// <end> FOR DEVELOPMENT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-	// <start> FOR PRODUCTION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	config_rds := Config{
-		DB_Username: "ownerwidho",
-		DB_Password: "kasircafeceria123",
+	config := Config{
+		DB_Username: "root",
+		DB_Password: "",
 		DB_Port:     "3306",
-		DB_Host:     "db-kasircafe.ctbpmx2n7zth.ap-southeast-2.rds.amazonaws.com",
-		DB_Name:     "db_kasircafe",
+		DB_Host:     "localhost",
+		DB_Name:     "kasircafe_db",
 	}
 
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config_rds.DB_Username,
-		config_rds.DB_Password,
-		config_rds.DB_Host,
-		config_rds.DB_Port,
-		config_rds.DB_Name,
+		config.DB_Username,
+		config.DB_Password,
+		config.DB_Host,
+		config.DB_Port,
+		config.DB_Name,
 	)
+	// <end> FOR DEVELOPMENT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	// <start> FOR PRODUCTION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	// config_rds := Config{
+	// 	DB_Username: "ownerwidho",
+	// 	DB_Password: "kasircafeceria123",
+	// 	DB_Port:     "3306",
+	// 	DB_Host:     "db-kasircafe.ctbpmx2n7zth.ap-southeast-2.rds.amazonaws.com",
+	// 	DB_Name:     "db_kasircafe",
+	// }
+
+	// connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	// 	config_rds.DB_Username,
+	// 	config_rds.DB_Password,
+	// 	config_rds.DB_Host,
+	// 	config_rds.DB_Port,
+	// 	config_rds.DB_Name,
+	// )
 	// <end> FOR PRODUCTION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 	var err error
